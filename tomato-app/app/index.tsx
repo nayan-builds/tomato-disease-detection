68cb9dbb64d8a8957fbeb77f2adcb5c2f256bbfe
nayan-builds <c2020083@hallam.shu.ac.uk>
2025-04-24 10:28:34 +0100

    Update home page

--- tomato-app/app/index.tsx
+++ tomato-app/app/index.tsx
@@ -12,42 +12,17 @@ export default function HomeScreen() {
   }, []);
   return (
     <ScrollView>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit{" "}
-          <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText>{" "}
-          to see changes. Press{" "}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: "cmd + d",
-              android: "cmd + m",
-              web: "F12",
-            })}
-          </ThemedText>{" "}
-          to open developer tools.
+      <ThemedView style={styles.container}>
+        <ThemedText style={styles.titleText} type="title">
+          Tomato-Scan
         </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this
-          starter app.
+        <ThemedText type="default">
+          This is where you can view the map of the disease locations from the
+          automatic disease identified from the "Map" tab.
         </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{" "}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText>{" "}
-          to get a fresh <ThemedText type="defaultSemiBold">app</ThemedText>{" "}
-          directory. This will move the current{" "}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{" "}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
+        <ThemedText type="default">
+          You can also view suggestions for each disease from the "Diseases"
+          tab.
         </ThemedText>
       </ThemedView>
     </ScrollView>
@@ -55,20 +30,16 @@ export default function HomeScreen() {
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: "row",
-    alignItems: "center",
-    gap: 8,
+  container: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "flex-start",
+    padding: 16,
+    width: "100%",
+    gap: 16,
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: "absolute",
+  titleText: {
+    textAlign: "center",
+    alignSelf: "center",
   },
 });
